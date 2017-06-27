f06c07224c94285f2d538bf01f28e8e000fc779b
Eduardo Vedes <eduardo.vedes@gmail.com>
2017-06-27 17:01:22 +0100

    routes done

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ const path = require('path')
 const multer = require('multer')
 var upload = multer({dest: 'uploads/'})
 
-
 // Route Definitions
 app.use('/', express.static(path.join(__dirname,'public')))
 
-app.post('/upload/', upload.single(), function(req,res,next) {
+app.post('/upload', upload.single('myFile'), function(req,res){
+    let name = req.file.originalname
+    let size = req.file.size
+    res.json({"File name":name, "File size":size})
     
 })
-
 
 //Server Listening and Port Definition
 const port = process.env.PORT | 8080;
