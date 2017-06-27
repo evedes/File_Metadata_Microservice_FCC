aa0881ee179475574c81601fa151d762395fdfa2
Eduardo Vedes <eduardo.vedes@gmail.com>
2017-06-27 14:06:59 +0100

    changed app.js to handle routes
    Updated the static path to public

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 // Express and other Requirements
 const express = require('express')
 const app = express()
+const path = require('path')
+const multer = require('multer')
+var upload = multer({dest: 'uploads/'})
+
+
+// Route Definitions
+app.use('/', express.static(path.join(__dirname,'public')))
+
+
+//Server Listening and Port Definition
 const port = process.env.PORT | 8080;
-const route = require('./routes/routes.js')
-
-
-// use routes.js file for routing
-app.use('/', route)
-
-
-//Server running and listening on const port
 app.listen(port,()=>{
     console.log('Server listening on port: ' + port)
 })
